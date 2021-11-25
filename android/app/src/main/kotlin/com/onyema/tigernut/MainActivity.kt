50f4873890525879c8f1773ceda72c2e746d28d3
OnyemaAnthony <Anthonychibuike246@gmail.com>
2021-11-25 13:53:45 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/onyema/tigernut/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.onyema.tigernut
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:tigernut/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tiger nut Application',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: HomeScreen(),
+    );
+
+  }
+}
